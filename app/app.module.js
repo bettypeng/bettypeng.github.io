bf673ae84cca84d2e20da0fdd6dea8cd3d887d04
Betty Peng <bettypeng@Bettys-MacBook-Pro.local>
2016-09-06 02:19:09 -0400

    Update app

--- app/app.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Define the `bettyApp` module
-angular.module('bettyApp', ['ngAnimate', 'ngRoute', 'projectDetail', 'projects', 'about', 'play']);
--- app/projects/projects.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Define the `projects` module
-angular.module('projects', []);
